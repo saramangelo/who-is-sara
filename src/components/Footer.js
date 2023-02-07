138ec053073231a64ee1ac11383c4a58ddcf1467
saramangelo <saramangelo@gmail.com>
2023-02-06 18:17:16 -0800

    added comments for each component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,3 +9,7 @@ function Footer() {
 }
 
 export default Footer;
+
+
+// WHEN I view the footer
+// THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) 
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,3 +9,6 @@ function Header() {
 }
 
 export default Header;
+
+// WHEN I view the header 
+// THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
