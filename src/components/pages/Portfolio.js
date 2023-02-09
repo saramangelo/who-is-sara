4ec2c6d3a1a364d09d7ff71084af669a9c1d05c1
saramangelo <saramangelo@gmail.com>
2023-02-09 02:04:35 -0800

    updated about me

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React from 'react';
 
-export default function Portfolio() {
+// TODO: CREATE PROJECT COMPONENT WHERE I PASS PROPS, MAP THROUGH TO PRINT PROJECT TILES HERE
+
+const Portfolio = ({ profiles, title }) => {
+  if (!profiles.length) {
+    return <h3>No Profiles Yet</h3>;
+  }
+
   return (
     <div>
-      <h1>Portfolio</h1>
-      <p>
-
-      </p>
+      <h3 className="text-primary">{title}</h3>
+      <div className="flex-row justify-space-between my-4">
+        {profiles &&
+          profiles.map((profile) => (
+            <div key={profile._id} className="col-12 col-xl-6">
+              <div className="card mb-3">
+                <h4 className="card-header bg-dark text-light p-2 m-0">
+                  {profile.name} <br />
+                  <span className="text-white" style={{ fontSize: '1rem' }}>
+                    currently has {profile.skills ? profile.skills.length : 0}{' '}
+                    endorsed skill
+                    {profile.skills && profile.skills.length === 1 ? '' : 's'}
+                  </span>
+                </h4>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Portfolio;
+
+
 
 
 // need to create array of objects that I map through to create a component for each element in the array | project tile for each project | NEED 6!!
