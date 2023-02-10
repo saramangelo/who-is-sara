42ffa027259224275596f251e179b90f61e3e3d4
saramangelo <saramangelo@gmail.com>
2023-02-10 09:49:03 -0800

    fixed issue with images

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,14 +9,14 @@ const Project = (props) => {
         <div className="project-card" key={generateKey(project.name)}>
           <img
             className="img-fluid shadow-2-strong rounded-4"
-            src={require(`../assets/images/${project.image}`).default}
+            src={require(`../assets/images/${project.image}`)}
             alt={project.description}
           ></img>
           <h2 className="card-title">{project.name}</h2>
           <div className="row icon-wrapper justify-content-end">
-            <a href={project.link} target="_blank" rel="noreferrer"></a>
+            <a href={project.link} target="_blank" rel="noreferrer">{project.link}</a>
 
-            <a href={project.githubLink} target="_blank" rel="noreferrer"></a>
+            <a href={project.githubLink} target="_blank" rel="noreferrer">{project.githubLink}</a>
           </div>
         </div>
       ))}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -9,7 +9,7 @@ const Portfolio = () => {
         "Kindly is a full-stack application where volunteer users can create an account, log in, submit available volunteer opportunities, photos, which are then posted to the homepage. Users can also comment on these posts.",
       link: "https://immense-ridge-33507.herokuapp.com/",
       githubLink: "https://github.com/saramangelo/kindly",
-      image: "../../assets/images/kindly.gif",
+      image: "kindly.jpg",
     },
     {
       name: "3Rex",
@@ -17,7 +17,7 @@ const Portfolio = () => {
         "This movie recommendation application features dynamically updated HyperText Markup Language (HTML) and Cascading Style Sheet (CSS) powered by JavaScript, third party Application Programming Interfaces (APIs), jQuery, Bootstrap, Google Fonts, featuring Server-Side APIs OMDb and YouTube.",
       link: "https://ben-jacquez.github.io/3Rex/",
       githubLink: "https://github.com/Ben-Jacquez/3Rex",
-      image: "../../assets/images/3Rex.gif",
+      image: "3Rex.jpg",
     },
     {
       name: "rain-check",
@@ -25,7 +25,7 @@ const Portfolio = () => {
         "This weather application, named rain-check, features dynamically updated HyperText Markup Language (HTML) and Cascading Style Sheet (CSS) powered by JavaScript and the third party Application Programming Interfaces (API), jQuery and Server-Side APIs Materialize, Google Fonts and Open Weathermap. In this application, the user is presented with current and future conditions for a specific city they search, and that city is added to the search history. Users can view current weather or a 5-day forecast for their searched city.",
       link: "https://saramangelo.github.io/rain-check/",
       githubLink: "https://github.com/saramangelo/rain-check",
-      image: "../../assets/images/raincheck.jpg",
+      image: "raincheck.jpg",
     },
     {
       name: "web-api-catechize",
@@ -33,7 +33,7 @@ const Portfolio = () => {
         "This Web API timed, multiple-choice quiz runs in the browser, featuring dynamically updated HTML and CSS, Third-Party Application Programming Interface, Bootstrap, powered by JavaScript.",
       link: "https://saramangelo.github.io/web-api-catechize/",
       githubLink: "https://github.com/saramangelo/web-api-catechize",
-      image: "../../assets/images/catechize.jpg",
+      image: "catechize.jpg",
     },
     {
       name: "password-generator",
@@ -41,7 +41,7 @@ const Portfolio = () => {
         "This password generator was originally derived from starter code with provided JavaScript (JS), HyperText Markup Language (HTML) and Cascading Style Sheets (CSS) files. The rendering was done solely within the JS file. In the webpage, when the user clicks the button, 'Generate Password,' the user is presented with a series of prompts, asking them to enter the number of characters they would like their password to be (between 8-128), and if they would like to include uppercase, lowercase, numbers and/or special characters. When the user answers each prompt, the information is saved, validated, then rendered on the screen, displaying a generated password that matches the user's selected criteria.",
       link: "https://saramangelo.github.io/password-generator/",
       githubLink: "https://github.com/saramangelo/password-generator",
-      image: "../../assets/images/passwordgenerator.jpeg",
+      image: "passwordgenerator.jpeg",
     },
     {
       name: "read-my-readme",
@@ -49,7 +49,7 @@ const Portfolio = () => {
         "This README generator is a command-line app application that accepts user input. While utilizing node, users are prompted for information about their application repository, which is then rendered in the form of a professional README.md with the title of their project and sections entitled Description, Table of Contents, Installation, Usage, License (including badge), Contributing, Tests, and Questions. When the user answers each prompt, the input is generated onto the README.md file in its respective location on the file.",
       link: "https://github.com/saramangelo/read-my-readme",
       githubLink: "https://github.com/saramangelo/read-my-readme",
-      image: "../../assets/images/readme.jpg",
+      image: "readme.jpg",
     },
   ];
   return (
