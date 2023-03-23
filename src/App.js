a283645e352ea88ccc5ff9d6df093f2c994a476f
saramangelo <saramangelo@gmail.com>
2023-03-22 18:16:21 -0700

    installed granim, updated App.js to include gradient background functionality and created necessary App.css to accompany. Need to edit navbar and footer next.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from "react";
+// import React from "react";
+// import PortfolioContainer from "./components/PortfolioContainer";
+
+
+// const App = () =>   <PortfolioContainer />
+
+
+// export default App;
+
+import React, { useEffect, useRef } from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-// import background from "./images/casey-horner-RmoWqDCqN2E-unsplash.jpg"
+import Granim from "granim";
+import "./App.css";
 
-// const styles = {
-//     image: {
-//     backgroundImage: `url(${background})`,
-//     height:'100vh',
-//     backgroundSize: 'cover',
-//     backgroundRepeat: 'no-repeat',
-// }
-// }
+const App = () => {
+  const canvasRef = useRef(null);
 
-// <div style={styles.image}></div>
+  useEffect(() => {
+    const granimInstance = new Granim({
+      element: canvasRef.current,
+      direction: "left-right",
+      opacity: [1, 1],
+      states: {
+        "default-state": {
+          gradients: [
+            ["#834d9b", "#d04ed6"],
+            ["#1d976c", "#93f9b9"],
+            ["#009ffd", "#2a2a72"]
+          ],
+          transitionSpeed: 2000
+        }
+      }
+    });
 
-const App = () =>   <PortfolioContainer />
+    return () => granimInstance.destroy(); // Clean up the instance on unmount
+  }, []);
 
+  return (
+    <>
+      <canvas id="logo-canvas" ref={canvasRef}></canvas>
+      <div className="app-container">
+        <PortfolioContainer />
+      </div>
+    </>
+  );
+};
 
 export default App;
-
