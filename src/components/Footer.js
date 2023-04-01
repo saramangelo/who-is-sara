c06a04869d4ad5c489a16f9f015f0ee10a0c9fb9
saramangelo <saramangelo@gmail.com>
2023-04-01 14:24:10 -0700

    adjusted spacing on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,8 @@ import {
   MDBIcon,
 } from "mdb-react-ui-kit";
 
+import "../index.css";
+
 export default function App() {
   return (
     <MDBFooter bgColor="" className="text-center text-lg-start text-muted">
@@ -18,17 +20,17 @@ export default function App() {
             <MDBRow className="">
               <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
                 <h6 className="text-uppercase fw-bold mb-4"> </h6>
-                <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+                <section className="icon-row">
                   <div className="me-5 d-none d-lg-block"></div>
 
-                  <div>
-                  <a
+                  <div className="footer-icons">
+                    <a
                       href="mailto:saramangelo@gmail.com"
                       className="me-4 text-reset"
                     >
                       <MDBIcon icon="envelope" size="lg" />
                     </a>
-                    
+
                     <a
                       href="https://github.com/saramangelo"
                       target="_blank"
@@ -61,17 +63,15 @@ export default function App() {
                     >
                       <MDBIcon fab icon="facebook-f" size="lg" />
                     </a>
-      
-      
-      
                   </div>
                 </section>
               </MDBCol>
             </MDBRow>
           </MDBContainer>
         </section>
-        <div class="footer-copyright text-center p-3 grey lighten-1 text-black-50">
+        <div class="footer-copyright text-center p-3 grey lighten-1 text-gray-50">
           © 2023 Copyright:
+          <br></br>
           <a
             id="footer-link-react-copyright"
             class="black-text"
