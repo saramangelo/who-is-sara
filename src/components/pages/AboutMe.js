150286a69f92dbae073f29743fff6e287ed28aba
saramangelo <saramangelo@gmail.com>
2023-04-01 13:35:37 -0700

    updated AboutMe.js page: adjusted sizing

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,40 +4,15 @@ import '../../App.css';
 export default function AboutMe() {
   return (
     <div className="aboutme-card ">
-      <h1 className="header">Sara Angelo</h1>
+      <h1 className="header mx-auto mt-5">Sara Angelo</h1>
       <div className="avatar">
         <Avatar />
       </div>
-
       <section
-        className="blurb card-body img-fluid align-items-center shadow-5-strong rounded-4"
-        style={{ width: 500 }}
+        // className="blurb card-body img-fluid align-items-center shadow-5-strong rounded-4"
+        // style={{ width: 500 }}
       >
-        <div>
-          <strong>Highly motivated</strong>, performance driven Full Stack
-          Developer with an accomplished, diverse background in web development,
-          healthcare, management, and financial securities.{" "}
-          <strong>Enthusiastic</strong> to work with new technologies and
-          architectures in a forward-thinking organization that’s always{" "}
-          <strong>pushing boundaries</strong>. Experience building products
-          across the stack with a firm understanding of web frameworks, APIs,
-          databases, and multiple back-end languages, protocols and standards.
-          Strong problem solving and analytical skills align with my proficiency
-          in{" "}
-          <strong>
-            HTML5, CSS3, JavaScript, jQuery, Express.js, React.js, Node.js,
-            progressive web apps, agile development methodologies, computer
-            science, database theory, MongoDB, MySQL, NoSQL, GraphQL, Git,
-            version control systems
-          </strong>
-          , and their respective best practices.
-        </div>
-      </section>
-      <section
-        className="blurb card-body img-fluid align-items-center shadow-5-strong rounded-4"
-        style={{ width: 500 }}
-      >
-        <div className="devicons">
+        <div className="devicons mb-3">
           <i class="devicon-html5-plain-wordmark colored"></i>
 
           <i class="devicon-css3-plain-wordmark colored"></i>
@@ -69,6 +44,30 @@ export default function AboutMe() {
           
         </div>
       </section>
+      <section
+        className="blurb card-body img-fluid align-items-center shadow-5-strong rounded-4"
+        style={{ width: 500 }}
+      >
+        <div>
+          <strong>Highly motivated</strong>, performance driven Full Stack
+          Developer with an accomplished, diverse background in web development,
+          healthcare, management, and financial securities.{" "}
+          <strong>Enthusiastic</strong> to work with new technologies and
+          architectures in a forward-thinking organization that’s always{" "}
+          <strong>pushing boundaries</strong>. Experience building products
+          across the stack with a firm understanding of web frameworks, APIs,
+          databases, and multiple back-end languages, protocols and standards.
+          Strong problem solving and analytical skills align with my proficiency
+          in{" "}
+          <strong>
+            HTML5, CSS3, JavaScript, jQuery, Express.js, React.js, Node.js,
+            progressive web apps, agile development methodologies, MongoDB, GraphQL, MySQL, NoSQL, Git,
+            version control systems
+          </strong>
+          , and their respective best practices.
+        </div>
+      </section>
+      
     </div>
   );
 }
