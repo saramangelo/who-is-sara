0f39728a32e5798d0a1f18d81e2f62a33be1688f
saramangelo <saramangelo@gmail.com>
2023-04-01 18:34:33 -0700

    adjusted resume h1

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,7 @@ import { MDBIcon } from "mdb-react-ui-kit";
 export default function Resume() {
   return (
     <div className="resume-container">
-      <h1 className="download">Download my resume</h1>
+      <h1 className="download d-flex text-center">Download my resume</h1>
       <p>
         <a
           href="https://docs.google.com/document/d/1kb-w-Lm-UAzbaKwu0JF8g2uUUa-oK4BA_LcK8BbNzUY/edit?usp=sharing"
